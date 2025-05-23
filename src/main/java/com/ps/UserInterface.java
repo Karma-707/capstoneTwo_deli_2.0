50d65dab21aa15b4ff05988cf89edc13c0748fb9
Seanna <meixinzhang1234@gmail.com>
2025-05-23 09:04:20 -0400

    completed Main, structured UserInterface

--- src/main/java/com/ps/UserInterface.java
+++ src/main/java/com/ps/UserInterface.java
@@ -1,4 +1,35 @@
 package com.ps;
 
+import com.ps.core.DeliStore;
+import com.ps.core.Product;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class UserInterface {
+    private static Scanner scanner = new Scanner(System.in);
+    private static DeliStore deliStore;
+    private static List<Product> shoppingCart;
+
+    //start to create a deli
+    public static void init() {
+        deliStore = new DeliStore();
+        display();
+    }
+
+    //TODO: deli main functions
+    private static void display() {
+
+    }
+
+    //TODO: add each order to user shopping
+    private static void addOrder() {
+
+    }
+
+    //TODO: checkout user items in shoppingCart
+    private static void checkOut() {
+
+    }
+
 }
